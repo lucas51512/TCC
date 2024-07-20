1eeb2c132db60d613a619a10163acb150b9f4fbc
Lucas <56892062+lucas51512@users.noreply.github.com>
2024-07-20 14:47:17 -0300

    Delete backend directory

--- backend/sparkApi/app/spark/database.py
+++
@@ -1,7 +0,0 @@
-import motor.motor_asyncio
-
-MONGO_DETAILS = "mongodb://localhost:27017"
-
-client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
-database = client.sparkDB
-collection = database.get_collection("spark_collection")
