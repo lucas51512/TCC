856b20f4876dc5a434d095ddffeab71dabc49725
lucas51512 <lucasbrancalhao454@gmail.com>
2024-07-22 00:23:53 -0300

    fix: some corrections

--- terraform/glue.tf
+++ terraform/glue.tf
@@ -2,7 +2,7 @@ resource "aws_glue_job" "glue_job" {
     name = "glue_script"
     role_arn = aws_iam_role.glue_job.arn
     glue_version = "3.0"
-    worker_type = "Standart"
+    worker_type = "Standard"
     number_of_workers = 2
     timeout = 5
    
